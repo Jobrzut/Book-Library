139ef1e91a75bedf26eb71e028940babe1c3ea22
Jobrzut <julek@geminidesign.pl>
2025-05-17 20:15:46 +0200

    Add a possibility to edit cards and display errors

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const closeModalButton = document.querySelector(".modal .close");
 const closeEditModalButton = document.querySelector(".modal__edit .close");
 const modal = document.querySelector(".modal");
 const statusSelect = document.querySelector(".status");
+const statusSelectEdit = document.querySelector(".statusEdit");
 const submitButton = document.querySelector(".submitButton");
 const form = document.querySelector("dialog form");
 
@@ -10,6 +11,7 @@ const titleInput = document.querySelector("#title");
 const authorInput = document.querySelector("#author");
 const totalPagesInput = document.querySelector("#totalPages");
 const readPagesInput = document.querySelector("#readPages");
+const readPagesEdit = document.querySelector(".readPagesEdit");
 const statusInput = document.querySelector("#status");
 
 const booksGrid = document.querySelector(".books__grid");
@@ -19,6 +21,7 @@ const readPagesCard = document.querySelector(".book__card input");
 let bookGridCards = document.querySelectorAll(".books__grid > div");
 
 const modalEdit = document.querySelector(".modal__edit");
+const submitModalEdit = document.querySelector(".submitEditButton")
 
 let books = [];
 
@@ -30,9 +33,87 @@ function noBooksToggle() {
     }
 }
 
+function checkMaxPages() {
+    function displayPagesError() {
+        if (+readPagesInput.value > +totalPagesInput.value && !modal.querySelector("div .errorInfo")) {
+            submitButton.disabled = true;
+            submitButton.style.cursor = "not-allowed";
+            let errorInfo = document.createElement("p");
+            errorInfo.className = "errorInfo";
+            errorInfo.style.color = "#8c3a3a";
+            errorInfo.style.marginTop = "5px";
+            errorInfo.textContent = "Impossible number of read pages..."
+            modal.querySelector("div").appendChild(errorInfo);
+        } else if (+readPagesInput.value <= +totalPagesInput.value && modal.querySelector("div .errorInfo")) {
+            submitButton.disabled = false;
+            submitButton.style.cursor = "pointer";
+            modal.querySelector("div .errorInfo").remove();
+        }
+    }
+    
+    readPagesInput.addEventListener("change", () => {
+        displayPagesError();
+    });
+    totalPagesInput.addEventListener("change", () => {
+        displayPagesError();
+        checkIfField();
+    })
+
+    titleInput.addEventListener("change", () => {
+        checkIfField();
+    })
+
+    authorInput.addEventListener("change", () => {
+        checkIfField();
+    })
+
+    function displayPagesErrorEdit() {
+        if (+readPagesEdit.value > +readPagesEdit.max && !modalEdit.querySelector("div .errorInfo")) {
+                submitModalEdit.disabled = true;
+                submitModalEdit.style.cursor = "not-allowed";
+                let errorInfo = document.createElement("p");
+                errorInfo.className = "errorInfo";
+                errorInfo.style.color = "#8c3a3a";
+                errorInfo.style.marginTop = "5px";
+                errorInfo.textContent = "Impossible number of read pages..."
+                modalEdit.querySelector("div").appendChild(errorInfo);
+        } else if (+readPagesEdit.value <= +readPagesEdit.max && modalEdit.querySelector("div .errorInfo")) {
+            submitModalEdit.disabled = false;
+            submitModalEdit.style.cursor = "pointer";
+            modalEdit.querySelector("div .errorInfo").remove();
+        }
+    }
+
+    readPagesEdit.addEventListener("change", () => {
+        displayPagesErrorEdit();
+    });
+}
+
+function checkIfField() {
+    if ((titleInput.value == "" || authorInput.value == "" || totalPagesInput.value == "") && !modal.querySelector(".requiredInfo") ) {
+        submitButton.disabled = true
+        submitButton.style.cursor = "not-allowed";
+        let errorInfo = document.createElement("p");
+        errorInfo.className = "requiredInfo";
+        errorInfo.style.color = "#8c3a3a";
+        errorInfo.style.marginTop = "5px";
+        errorInfo.textContent = "Fill all the required fields!"
+        modal.querySelector("div").appendChild(errorInfo);
+    } else if (titleInput.value !== "" && authorInput.value !== "" && totalPagesInput.value !== "" && modal.querySelector(".requiredInfo")) {
+        submitButton.disabled = false;
+        submitButton.style.cursor = "pointer";
+        modal.querySelector(".requiredInfo").remove();
+    }
+}
+
 
 function ShowHideModal() {
-    addBooksButton.addEventListener("click", () => modal.showModal());
+    addBooksButton.addEventListener("click", () => { 
+        modal.showModal()
+        if (modal.querySelector(".requiredInfo")) {
+            modal.querySelector(".requiredInfo").remove();
+        }
+    });
     closeModalButton.addEventListener("click", () => modal.close());
     modal.addEventListener('click', function(e) {
         if (e.target === e.currentTarget) {
@@ -54,6 +135,16 @@ function Book(title, author, totalPages, readPages, status)  {
     this.id = crypto.randomUUID();
 }
 
+Book.prototype.changeStatus = function(status, readPages) {
+    this.status = status;
+    if (status == "Reading") {
+        this.readPages = readPages;
+        if (this.readPages == this.totalPages) {
+            this.status = "Read";
+        }
+    }
+}
+
 function EnableDisableInput() {
     statusSelect.addEventListener("change", (e) => {
         if (e.target.value == "Reading") {
@@ -67,16 +158,17 @@ function EnableDisableInput() {
 function addBookToLibrary() {
     submitButton.addEventListener("click", (e) =>{
         e.preventDefault();
-        if (titleInput.value !== "" && authorInput.value !== "") {
+        if (titleInput.value !== "" && authorInput.value !== "" && totalPagesInput.value !== "") {
             books.push(new Book(titleInput.value, authorInput.value, totalPagesInput.value, readPagesInput.value, statusInput.value));
-        }
-        modal.close();
-        form.reset();
-        readPagesInput.disabled = true;
-        displayBooks();
-        bookGridCards = document.querySelectorAll(".books__grid > div");
-        removeCard();
-        editCard();
+            modal.close();
+            form.reset();
+            readPagesInput.disabled = true;
+            displayBooks();
+            bookGridCards = document.querySelectorAll(".books__grid > div");
+            removeCard();
+            editCard();
+        } 
+        checkIfField();
     });
 }
 
@@ -115,7 +207,13 @@ function displayBooks() {
             if (book.status == "Reading") {
                 const readPages = document.createElement("p");
                 readPages.textContent = "Read pages: " + book.readPages;
+                readPages.className = "read__pages";
                 bookCardText.appendChild(readPages);
+
+                const status = document.createElement("p");
+                status.textContent = "Status: " + book.status;
+                status.className = "status";
+                bookCardText.appendChild(status);
 
                 const progressBar = document.createElement("progress");
                 progressBar.id = "readprogress";
@@ -125,6 +223,7 @@ function displayBooks() {
             } else {
                 const status = document.createElement("p");
                 status.textContent = "Status: " + book.status;
+                status.className = "status";
                 bookCardText.appendChild(status);
             }
             
@@ -167,12 +266,82 @@ function editCard() {
     let editButtons = document.querySelectorAll(".edit__button");
     if (editButtons) {
         editButtons.forEach((button) => {
-            button.addEventListener("click", (e) => modalEdit.showModal());
+            button.addEventListener("click", (e) => { 
+                modalEdit.showModal()
+                const statusEditInput = document.querySelector(".modal__edit .statusEdit");
+                let currentCard = button.parentElement.parentElement.id;
+                let indexOfCard = books.findIndex(book => book.id == currentCard);
+                statusEditInput.value = books[indexOfCard].status;
+                const readEditInput = document.querySelector(".modal__edit .readPagesEdit");
+                readEditInput.max = books[indexOfCard].totalPages;
+                if (statusEditInput.value == "Reading") {
+                    readPagesEdit.disabled = false;
+                    readEditInput.value = books[indexOfCard].readPages;
+                }
+            });
             closeEditModalButton.addEventListener("click", () => modalEdit.close());
+            modalEdit.addEventListener('click', function(e) {
+                if (e.target === e.currentTarget) {
+                e.stopPropagation();
+                modalEdit.close();
+                }
+            });
+            let editedCard = button.parentElement.parentElement
+            let currentCardId = editedCard.id;
+            let indexOfCard = books.findIndex(book => book.id == currentCardId);
+            editBooks(indexOfCard, editedCard);
+        });
+    }
+}
+
+function EnableDisableEditInput() {
+    statusSelectEdit.addEventListener("change", (e) => {
+        if (e.target.value == "Reading") {
+            readPagesEdit.disabled = false;
+        } else {
+            readPagesEdit.disabled = true;
+            readPagesEdit.value = "";
+        }
     });
 }
+
+function editBooks(indexOfCard, editedCard) {
+    let submitEditButton = document.querySelector(".submitEditButton");
+    submitEditButton.addEventListener("click", (e) => {
+        e.preventDefault();
+        modalEdit.close();
+        books[indexOfCard].changeStatus(statusSelectEdit.value,readPagesEdit.value);
+        editedCard.querySelector(".status").textContent = "Status: " + books[indexOfCard].status;
+        if (books[indexOfCard].status == "Reading") {
+            if(editedCard.querySelector("progress")) {
+                editedCard.querySelector("progress").remove();
+            }
+            if(editedCard.querySelector(".read__pages")) {
+                editedCard.querySelector(".read__pages").remove();
+            }
+            
+            const readPages = document.createElement("p");
+            readPages.className = "read__pages";
+            readPages.textContent = "Read pages: " + books[indexOfCard].readPages;
+            editedCard.querySelector(".book__card_text").appendChild(readPages);
+            const progressBar = document.createElement("progress");
+            progressBar.id = "readprogress";
+            progressBar.value = books[indexOfCard].readPages;
+            progressBar.max = books[indexOfCard].totalPages;
+            editedCard.querySelector(".book__card_text").appendChild(progressBar);
+        } else {
+            if(editedCard.querySelector("progress")) {
+                editedCard.querySelector("progress").remove();
+            }
+            if(editedCard.querySelector(".read__pages")) {
+                editedCard.querySelector(".read__pages").remove();
+            }
+        }
+    });
 }
 
 ShowHideModal();
 EnableDisableInput();
 addBookToLibrary();
+EnableDisableEditInput();
+checkMaxPages();
